5e387c54d6557dff5424af0bfc53a807928cac81
ellscha <escharlin@wesleyan.edu>
2016-09-03 12:32:23 -0400

    First Commit

---
+++ DogMatched/Keys.swift
@@ -0,0 +1,12 @@
+//
+//  Keys.swift
+//  DogMatched
+//
+//  Created by Elli Scharlin on 9/3/16.
+//  Copyright © 2016 ElliScharlin. All rights reserved.
+//
+
+import Foundation
+struct Keys {
+    static let test = "TEST"
+}
---
+++ DogMatched/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DogMatched
+//
+//  Created by Elli Scharlin on 9/3/16.
+//  Copyright © 2016 ElliScharlin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
